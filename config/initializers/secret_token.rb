fa4b8ff8f71223534f3e421b9995a94ee36fc88e
Ajmal Afif <ajmalafif@gmail.com>
2012-02-05 17:47:55 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VuApp::Application.config.secret_token = '9ba3c28356ca5e1f0970bc39c6ae00ddc3733f0a32283da34d5501f280fd221b49c918ee0c859ee0ad394a01c9e56f38723eadbc06ab1638d1f3c7986a5a57f8'
